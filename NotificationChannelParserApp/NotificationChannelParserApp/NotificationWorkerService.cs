ba629f115f8b8517ca9721a44d7fff4d4bdc44c4
Lida <y_uguest@yahoo.co.uk>
2024-11-05 10:16:09 +0700

    first commit

---
+++ NotificationChannelParserApp/NotificationChannelParserApp/NotificationWorkerService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace NotificationChannelParserApp
+{
+    public class NotificationWorkerService : IHostedService
+    {
+        private readonly Channel<string> _channel;
+        private readonly ILogger _logger;
+        private string respoonse = "Receive channels:";
+
+        public NotificationWorkerService(ILogger<NotificationWorkerService> logger,
+ IHostApplicationLifetime appLifetime, Channel<string> channel)
+        {
+            _channel = channel;
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.Factory.StartNew(async () =>
+            {
+                while (await _channel.Reader.WaitToReadAsync())
+                {
+                    while (_channel.Reader.TryRead(out string? text))
+                    {
+                        respoonse += text + ",";
+                    }
+                    Console.WriteLine(respoonse.Substring(0, respoonse.Length - 1));
+                }
+
+            });
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ NotificationChannelParserApp/NotificationChannelParserApp/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NotificationChannelParserApp;
+using System.Threading.Channels;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<NotificationWorkerService>();
+builder.Services.AddLogging(logBuiler =>
+{
+    logBuiler.ClearProviders();
+    logBuiler.AddConsole();
+});
+var channelBE = Channel.CreateUnbounded<string>();
+
+//[BE][FE][Urgent] there is error
+Console.Write("Input: ");
+var input = Console.ReadLine();
+
+if (input.IndexOf("[BE]") >= 0) await channelBE.Writer.WriteAsync("BE");
+if (input.IndexOf("[FE]") >= 0) await channelBE.Writer.WriteAsync("FE");
+if (input.IndexOf("[QA]") >= 0) await channelBE.Writer.WriteAsync("QA");
+if (input.IndexOf("[Urgent]") >= 0) await channelBE.Writer.WriteAsync("Urgent");
+
+builder.Services.AddSingleton(channelBE);
+
+IHost app = builder.Build();
+
+await app.RunAsync();
+
